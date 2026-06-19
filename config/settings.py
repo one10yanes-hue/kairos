b4a6856f6cdacba3bc3cbf77c4f2864070f94d74
Humberto Yanes <hyanes@DOMAIN.LOCAL>
2026-06-19 09:43:00 -0500

    v4.2.4: Fix ALLOWED_HOSTS default as list

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ environ.Env.read_env(env_file)
 
 SECRET_KEY = env("SECRET_KEY")
 DEBUG = env.bool("DEBUG", default=False)
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default="localhost,127.0.0.1,testserver")
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1", "testserver"])
 
 INSTALLED_APPS = [
     "apps.accounts",
