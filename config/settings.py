bb87cdc4b328533e190126189b99e7f0b4e95015
Humberto Yanes <hyanes@DOMAIN.LOCAL>
2026-06-22 14:28:30 -0500

    v4.2.4: Hotfix - quitar proyecto_contexto de TEMPLATES context_processors (no existe en produccion)

--- config/settings.py
+++ config/settings.py
@@ -62,7 +62,6 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "apps.proyectos.context_processors.proyecto_contexto",
             ],
         },
     },
